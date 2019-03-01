f2971959358f124cd171714ecc2aca7f41814619
Ananth Tatachar <ananth.tatachar@gmail.com>
2019-02-28 20:12:23 -0500

    small changes.

---
+++ LinkedLists/ITwoNumberAddition.cs
@@ -0,0 +1,9 @@
+using LeetCode.Models;
+
+namespace LeetCode.LinkedLists
+{
+    public interface ITwoNumberAddition
+    {
+        ListNode AddTwoNumbers(ListNode l1, ListNode l2);
+    }
+}
---
+++ LinkedLists/TwoNumberAddition.cs
@@ -0,0 +1,11 @@
+using LeetCode.Models;
+
+namespace LeetCode.LinkedLists
+{
+    public class TwoNumberAddition : ITwoNumberAddition
+    {
+        public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+        {
+        }
+    }
+}
